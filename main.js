cbbac3501f197f897ef50dd163fa805e4b0428c3
Alex Sanchez <alesanchez728@gmail.com>
2017-08-22 18:53:52 -0500

    added some dom-traversal knowledge

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ allProducts.push(product2);
 allProducts.push(product3);
 allProducts.push(product4);
 allProducts.push(product5);
-console.log("all my baby products", allProducts);
 
 var productContainer = document.getElementById("product-container");
 
@@ -58,18 +57,18 @@ function buildDomString(product) {
   var domString = "";
 
   domString +=  '<section class="product">';
-  domString +=        '<div class="title">';
+  domString +=        '<div class="title child">';
   domString +=          '<h2>' + product.name +'</h2>';
   domString +=        '</div>';
-  domString +=        '<div class="image">';
+  domString +=        '<div class="image child">';
   domString +=          '<img src="' + product.imagePath +'" alt="' + product.imageAlt +'">';
   domString +=        '</div>';
-  domString +=        '<div class="description">';
+  domString +=        '<div class="description child">';
   domString +=          '<p>' + product.description +'</p>';
   domString +=          '<h6>$' + product.price +'</h6>';
   domString +=        '</div>';
   if (product.soldOut) {
-    domString +=      '<div class="sold-out">';
+    domString +=      '<div class="sold-out child">';
     domString +=        '<img src="./images/soldOut.png" alt="Sold Out">';
     domString +=      '</div>';
   }
@@ -85,11 +84,35 @@ function printProductArrayToDom(productArray) {
     var productDomString = buildDomString(currentProduct);
 
       // print out domString that looped through the function
-    console.log("Dom String from for loop", productDomString)
+    // console.log("Dom String from for loop", productDomString)
     productContainer.innerHTML += productDomString;
   }
 }
 printProductArrayToDom(allProducts);
+
+var selectedCard;
+
+document.getElementById("product-container").addEventListener("click", function(event){
+  // console.log(event.target)
+  changeBorder(event);
+  printSelectedDescription();
+});
+function changeBorder(event) {
+
+  if (event.target.classList.contains("child")) {
+    selectedCard = event.target.parentNode;
+  } else if (event.target.parentNode.parentNode.classList.contains("product")){
+    selectedCard = event.target.parentNode.parentNode;
+  } else if (event.target.classList.contains("product")) {
+    selectedCard = event.target;
+  }
+  selectedCard.classList.add("boarderPatrol");
+}
+function printSelectedDescription() {
+  var description = selectedCard.childNodes[2].childNodes[0].innerHTML;
+  console.log(description);
+}
+
 
 
 
